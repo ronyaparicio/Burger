9a5d34bde70e17eb5cf1d6b86d130634888384dd
rony <ronyaparicio1@gmail.comgit config --global user.name ronygit commit -m 'l'git config --global user.email ronyaparicio1@gmail.com>
2017-09-27 20:16:06 -0600

    idk

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,12 +1,12 @@
 var express = require("express");
 var router = express.Router();
 
-var cat = require("../models/burger.js");
+var burger = require("../models/burger.js");
 
 router.get("/", function(req, res) {
-  cat.all(function(data) {
+  burger.all(function(data) {
     var hbsObject = {
-      cats: data
+      burger: data
     };
     console.log(hbsObject);
     res.render("index", hbsObject);
@@ -15,4 +15,4 @@ router.get("/", function(req, res) {
 
 router.get("/", function(req,res) {
 
-})
+});
--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var methodOverride = require("method-override");
 var port = process.env.PORT || 3000;
 var app = express();
 
-// Serve static content for the app from the "public" directory in the application directory.
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: false }));
 
