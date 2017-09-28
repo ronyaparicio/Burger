5244222fda3f0450c91c3c6d3d2aee706ecec190
Rony Aparicio <u6009311@ad.utah.edu>
2017-09-28 10:58:42 -0600

    isk

--- models/burger.js
+++ models/burger.js
@@ -1,7 +1,6 @@
 var orm = require("../config/orm.js");
 
 var burger = {
-<<<<<<< HEAD
     selectAll: function() {
         orm.selectAll('burgers',) 
 
@@ -10,9 +9,3 @@ var burger = {
 }
 
 module.exports = burger;
-=======
-    all: function() {
-        orm.selectAll('burgers','devoured',0)
-    }
-}
->>>>>>> 7fd811eb017f259e5987ab803fafcdad11a5f454
